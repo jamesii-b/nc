3fb3504052d54ab84df70c6b0ab30e1f0af49fe8
James Bhattarai <jamesbhattarai14@gmail.com>
2024-02-26 16:25:17 +0545

    Chore: added testimonial at hero

---
+++ src/components/homePage/components/skillSection copy.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaQuoteLeft } from "react-icons/fa";
+import testimonialImg from "../../../assets/testimonials/h1.png"; // Import the image of the testimonial giver
+
+// Sample testimonial data
+const testimonial = {
+  quote: "We are very happy with the Development Services received from Nepcodes. The young team is incredibly talented and attentive detail which made us achieve the website that we envisioned of.",
+  author: "Krishna Neupane",
+  position: "EEBSS",
+  image: testimonialImg // Image of the testimonial giver
+};
+
+function TestimonialSection() {
+  return (
+    <section className="py-4 ">
+      <div className="container mx-auto px-4">
+        <div className="max-w-3xl mx-auto text-center bg-white rounded-lg shadow-lg p-8">
+          <FaQuoteLeft className="text-3xl text-blue-500 mb-4 mx-auto" />
+          <p className="text-lg text-gray-800 leading-relaxed mb-6">{testimonial.quote}</p>
+          <div className="flex flex-col items-center">
+            <img src={testimonial.image} alt={testimonial.author} className="w-20 h-20 object-cover rounded-full mb-4" />
+            <p className="text-lg font-semibold text-gray-800">{testimonial.author}</p>
+            <p className="text-gray-600">{testimonial.position}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TestimonialSection;
