347dcc553932ee7f992fafbe3e80b6d018b8a4d6
James Bhattarai <jamesbhattarai14@gmail.com>
2024-02-29 09:06:47 +0545

    Chore: trivial nav-bar changes

--- src/components/reusable/navbar.jsx
+++ src/components/reusable/navbar.jsx
@@ -27,8 +27,8 @@ function NavBar() {
   const handleLeave = () => setShowCategories(false);
 
   return (
-    <nav className={`fixed w-full text-secondary_text z-50 h-20 top-0 ${scrolled ? "bg-secondary" : "bg-transparent"}`}>
-      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+    <nav className={`fixed w-full text-secondary_text z-50 text-xl h-20 top-0 ${scrolled ? "bg-secondary" : "bg-transparent"}`}>
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center text-t_secondary">
         <div className="px-8 space-x-8">
           <button className="lg:hidden md:hidden text-2xl text-blue-300 focus:outline-none" onClick={toggleMenu}>
             {showMenu ?
@@ -36,21 +36,21 @@ function NavBar() {
             }
           </button>
           <div className="hidden lg:block md:block ">
-            <Link to="/" className={`text-lg font-semibold mr-10 ${location.pathname === "/" ? "text-blue-300 overline leading-loose" : "hover:text-blue-300"}`}>Home</Link>
-            <Link to="/what-we-do" onMouseEnter={handleHover} onMouseLeave={handleLeave} className={`mr-10 text-lg font-semibold relative ${location.pathname.startsWith("/what-we-do") ? "text-blue-300 overline leading-loose" : "hover:text-blue-300"}`}>
+            <Link to="/" className={`  mr-10 ${location.pathname === "/" ? "text-primary font-extrabold  overline  leading-loose" : "hover:text-blue-300"}`}>Home</Link>
+            <Link to="/services" onMouseEnter={handleHover} onMouseLeave={handleLeave} className={`mr-10   relative ${location.pathname.startsWith("/services") ? "text-primary font-extrabold  overline  leading-loose" : "hover:text-blue-300"}`}>
               {showCategories && (
                 <span className="absolute top-full left-0 max-w-full lg:max-w-md -ml-4 bg-blue-100 bg-opacity-80 p-4 rounded-md">
                   <div className="w-full lg:w-96 flex">
-                    <Link to="/services/development" className="text-lg mr-6 font-semibold block mb-4 pr-6 border-r-2   border-gray-600">Development<div className="text-sm text-black mt-2">We provide web development services that are scalable, secure, and reliable.</div></Link>
-                    <Link to="/services/ui-ux" className="text-lg font-semibold block">UI/UX Design<div className="text-sm text-black mt-2">We design user-friendly, intuitive, and visually appealing interfaces that enhance user experience.</div></Link>
+                    <Link to="/services/development" className=" mr-6  block mb-4 pr-6 border-r-2   border-gray-600">Development<div className="text-sm text-black mt-2">We provide web development services that are scalable, secure, and reliable.</div></Link>
+                    <Link to="/services/ui-ux" className="  block">UI/UX Design<div className="text-sm text-black mt-2">We design user-friendly, intuitive, and visually appealing interfaces that enhance user experience.</div></Link>
                   </div>
                 </span>
               )}
               Services
             </Link>
-            <Link to="/about" className={`mr-10 text-lg font-semibold ${location.pathname === "/about" ? "text-blue-300 overline leading-loose" : "hover:text-blue-300"}`}>About</Link>
-            <Link to="/blogs" className={`mr-10 text-lg font-semibold ${location.pathname === "/blogs" ? "text-blue-300 overline leading-loose" : "hover:text-blue-300"}`}>Blogs</Link>
-            <Link to="/faqs" className={`mr-10 text-lg font-semibold ${location.pathname === "/faqs" ? "text-blue-300 overline leading-loose" : "hover:text-blue-300"}`}>FAQs</Link>
+            <Link to="/about" className={`mr-10   ${location.pathname === "/about" ? "text-primary font-extrabold  overline  leading-loose" : "hover:text-blue-300"}`}>About</Link>
+            <Link to="/blogs" className={`mr-10   ${location.pathname === "/blogs" ? "text-primary font-extrabold  overline  leading-loose" : "hover:text-blue-300"}`}>Blogs</Link>
+            <Link to="/faqs" className={`mr-10   ${location.pathname === "/faqs" ? "text-primary font-extrabold  overline  leading-loose" : "hover:text-blue-300"}`}>FAQs</Link>
           </div>
         </div>
         <div className="hidden lg:block md:block">
@@ -59,24 +59,24 @@ function NavBar() {
       </div>
       {
         showMenu && (
-          <div className="lg:hidden sm:visible md:hidden bg-blue-100 p-7 rounded-md">
+          <div className="lg:hidden sm:visible md:hidden font-medium bg-blue-100 p-7 rounded-md">
             <div className="flex flex-col space-y-4">
-              <Link to="/" className="text-lg font-semibold py-1 px-4 rounded-md ">Home</Link>
+              <Link to="/" className="  py-1 px-4 rounded-md ">Home</Link>
               <hr className="border-blue-200 my-2" />
               <div className="flex flex-col">
                 <div className="flex items-center">
-                  <Link to="/what-we-do" className="text-lg font-semibold py-1 px-4 rounded-md ">Services</Link>
+                  <Link to="/services" className="  py-1 px-4 rounded-md ">Services</Link>
                   <FaAngleDown className="text-2xl ml-2" />
                 </div>
                 <div className="pl-8">
-                  <Link to="/services/development" className="text-lg font-semibold block py-1 px-4 rounded-md hover:bg-blue-200">Web Development</Link>
-                  <Link to="/services/ui-ux" className="text-lg font-semibold block py-1 px-4 rounded-md ">UI/UX Design</Link>
+                  <Link to="/services/development" className="  block py-1 px-4 rounded-md">Web Development</Link>
+                  <Link to="/services/ui-ux" className="  block py-1 px-4 rounded-md ">UI/UX Design</Link>
                 </div>
               </div>
               <hr className="border-blue-200 my-2" />
-              <Link to="/about" className="text-lg font-semibold py-1 px-4 rounded-md ">About</Link>
-              <Link to="/blog" className="text-lg font-semibold py-1 px-4 rounded-md ">Blogs</Link>
-              <Link to="/faqs" className="text-lg font-semibold py-1 px-4 rounded-md ">FAQs</Link>
+              <Link to="/about" className="  py-1 px-4 rounded-md ">About</Link>
+              <Link to="/blog" className="  py-1 px-4 rounded-md ">Blogs</Link>
+              <Link to="/faqs" className="  py-1 px-4 rounded-md ">FAQs</Link>
               <hr className="border-blue-200 my-2" />
               <Link to="/contact">
                 <button className="bg-blue-400 text-gray-800 py-2 px-4 rounded-md ">Work With Us</button>
